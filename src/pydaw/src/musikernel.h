7a34f0a500e2c39840287a3ffc3ef80ed2ad3f9d
MusiKernel Team <musikernel@nospam.org>
2014-10-16 19:53:36 -0400

    Add the proper license header to musikernel.h

--- src/pydaw/src/musikernel.h
+++ src/pydaw/src/musikernel.h
@@ -1,9 +1,15 @@
 /*
- * File:   musikernel.h
- * Author: userbuntu
- *
- * Created on September 28, 2014, 6:28 PM
- */
+This file is part of the MusiKernel project, Copyright MusiKernel Team
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
 
 #ifndef MUSIKERNEL_H
 #define	MUSIKERNEL_H
