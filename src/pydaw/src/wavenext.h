fecab66a6d658432023ad0fc8efb255e77c53faf
MusiKernel Team <musikernel@nospam.org>
2014-10-22 18:32:32 -0400

    Update the copyright notice on various files...

--- src/pydaw/src/wavenext.h
+++ src/pydaw/src/wavenext.h
@@ -1,9 +1,15 @@
 /*
- * File:   wavenext.h
- * Author: userbuntu
- *
- * Created on September 28, 2014, 6:30 PM
- */
+This file is part of the MusiKernel project, Copyright MusiKernel Team
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
 
 #ifndef WAVENEXT_H
 #define	WAVENEXT_H
