f327a042d32003d98a2afa3cd5376ed3db970608
MusiKernel Team <musikernel@nospam.org>
2014-11-20 21:51:50 -0500

    Add the new folder to the lines-of-code counting script

--- src/loc.sh
+++ src/loc.sh
@@ -4,6 +4,7 @@ pydaw/libmodsynth/lib/* pydaw/libmodsynth/*/*/*
 
 echo "^^^ Lines of C code"
 
-wc -l pydaw/python/*.py pydaw/python/libpydaw/*.py pydaw/python/mkplugins/*
+wc -l pydaw/python/*.py pydaw/python/libpydaw/*.py \
+pydaw/python/libmk/*.py pydaw/python/mkplugins/*
 
 echo "^^^ Lines of Python code"
