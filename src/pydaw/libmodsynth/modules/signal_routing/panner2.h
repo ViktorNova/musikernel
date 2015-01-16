3bd37e72d59d5714a2a5d0dcf9510ecd0cc44aeb
MusiKernel Team <musikernel@nospam.org>
2015-01-16 08:36:05 -0500

    Fix channel bleed in the crossfader

--- src/pydaw/libmodsynth/modules/signal_routing/panner2.h
+++ src/pydaw/libmodsynth/modules/signal_routing/panner2.h
@@ -41,6 +41,16 @@ void v_pn2_set(t_pn2_panner2 * self, float a_pan, float a_law)
         self->gainL = f_db_to_linear_fast(a_law);
         self->gainR = self->gainL;
     }
+    else if(a_pan == -1.0f)
+    {
+        self->gainL = 1.0f;
+        self->gainR = 0.0f;
+    }
+    else if(a_pan == 1.0f)
+    {
+        self->gainL = 0.0f;
+        self->gainR = 1.0f;
+    }
     else if(a_pan < 0.0f)
     {
         self->gainL = f_db_to_linear_fast((1.0f + a_pan) * a_law);
