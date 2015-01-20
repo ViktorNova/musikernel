e55e048d25329fb464cf3b837584b3aa09f42f1f
MusiKernel Team <musikernel@nospam.org>
2015-01-20 18:54:09 -0500

    Create a script for building the application for Cygwin

---
+++ cygwin.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+This file is part of the MusiKernel project, Copyright MusiKernel Team
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+import os
+import shutil
+
+with open("src/major-version.txt") as f_file:
+    MAJOR_VERSION = f_file.read().strip()
+
+DESTDIR = os.path.join("", MAJOR_VERSION)
+
+if os.path.isdir(DESTDIR):
+    OLD_DIR = "{}-OLD".format(DESTDIR)
+    if os.path.isdir(OLD_DIR):
+        shutil.rmtree(OLD_DIR)
+    shutil.move(DESTDIR, OLD_DIR)
+else:
+    OLD_DIR = None
+
+f_cwd = os.path.join(os.path.abspath(os.path.dirname(__file__)), "src")
+
+os.chdir(f_cwd)
+
+f_retcode = os.system(
+    "make clean && make && make DESTDIR={} install".format(DESTDIR))
+
+if(f_retcode):
+    print("Error:  clean, build and install returned {}".format(f_retcode))
