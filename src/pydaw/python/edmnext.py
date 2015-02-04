2a6e7a0db5a699f6b7111fc653e72913652332b5
MusiKernel Team <musikernel@nospam.org>
2015-02-03 19:47:45 -0500

    More Qt5 fixes

--- src/pydaw/python/edmnext.py
+++ src/pydaw/python/edmnext.py
@@ -2884,7 +2884,7 @@ class audio_viewer_item(QGraphicsRectItem):
                         self.sample_start_offset_px +
                         self.length_seconds_orig_px,
                         self.y_inc + (f_y_offset * -1.0) + (f_y_inc * f_i))
-                    f_path_item.rotate(-180.0)
+                    f_path_item.setRotation(-180.0)
                 else:
                     f_path_item.setPos(
                         self.sample_start_offset_px,
