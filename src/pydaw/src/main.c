785f10b3d951db48a8a05cdb4797b97c48b2403a
MusiKernel Team <musikernel@nospam.org>
2015-02-13 22:31:52 -0500

    Fix the hang on exit (for reasons I don't understand) and maybe fix one of the last pieces of the Windows puzzle (still being determined)

--- src/pydaw/src/main.c
+++ src/pydaw/src/main.c
@@ -92,16 +92,12 @@ int osc_debug_handler(const char *path, const char *types, lo_arg **argv, int
 		      argc, void *data, void *user_data) ;
 #endif
 
-#ifdef	__cplusplus
-extern "C" {
+
+#ifdef WIN32
+int __declspec(dllexport) main(int argc, char **argv);
+int __declspec(dllexport) v_configure(char * path, char * key, char * value);
 #endif
 
-int main(int argc, char **argv);
-int v_configure(char * path, char * key, char * value);
-
-#ifdef __cplusplus
-}
-#endif
 
 inline void v_pydaw_run_main_loop(int sample_count,
         float **output, float *a_input_buffers);
