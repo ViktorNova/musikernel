e5e3193b511a648b3941713970de901bb4d18981
MusiKernel Team <musikernel@nospam.org>
2015-02-20 07:30:29 -0500

    Fix missing backslash (when did that happen?)

--- src/ubuntu_deps.sh
+++ src/ubuntu_deps.sh
@@ -8,7 +8,7 @@ fi
 apt-get update
 
 apt-get install -y libasound2-dev \
-libportmidi-dev portaudio19-dev liblo-dev g++ libsndfile1-dev 
+libportmidi-dev portaudio19-dev liblo-dev g++ libsndfile1-dev \
 libtool gdb debhelper dh-make build-essential automake autoconf \
 python3-pyqt5 python3 squashfs-tools genisoimage \
 python3-scipy python3-numpy libsamplerate0-dev \
