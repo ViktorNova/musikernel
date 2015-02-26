868ac2b706ad80a1099b49e6a5588168c3151f38
MusiKernel Team <musikernel@nospam.org>
2015-02-25 21:29:29 -0500

    Remove Scipy and libsamplerate as dependencies, they are no longer used

--- src/ubuntu_deps.sh
+++ src/ubuntu_deps.sh
@@ -11,7 +11,6 @@ apt-get install -y libasound2-dev \
 libportmidi-dev portaudio19-dev liblo-dev g++ libsndfile1-dev \
 libtool gdb debhelper dh-make build-essential automake autoconf \
 python3-pyqt5 python3 squashfs-tools genisoimage \
-python3-scipy python3-numpy libsamplerate0-dev \
-libfftw3-dev gcc python3-dev libsbsms-dev libcpufreq-dev \
+python3-numpy libfftw3-dev gcc python3-dev libsbsms-dev libcpufreq-dev \
 libav-tools lame vorbis-tools gettext rubberband-cli 
 
