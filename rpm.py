302722c5ac0459f4429b8fb998d93501efc8bbea
MusiKernel Team <musikernel@nospam.org>
2015-05-07 19:31:18 -0400

    Do the RPM spec file "the right way" so that hopefully COPR builds will start working...

--- rpm.py
+++ rpm.py
@@ -93,7 +93,7 @@ Requires:       python3-qt5 alsa-lib-devel liblo-devel rubberband \
 libsndfile-devel python3-numpy fftw-devel portmidi-devel \
 portaudio-devel python3-devel vorbis-tools
 
-%define __provides_exclude_from ^/usr/lib/{0}/.*$
+%define __provides_exclude_from ^%{{_libdir}}/{0}/.*$
 
 %description
 MusiKernel is digital audio workstations (DAWs), instrument and effect plugins
@@ -116,216 +116,216 @@ rm -rf $RPM_BUILD_ROOT
 
 %defattr(644, root, root)
 
-%attr(4755, root, root) /usr/bin/{0}-engine
+%attr(4755, root, root) %{{_bindir}}/{0}-engine
 
-%attr(755, root, root) /usr/bin/{0}
-%attr(755, root, root) /usr/bin/{0}_render
-%attr(755, root, root) /usr/bin/{0}_render-dbg
-%attr(755, root, root) /usr/bin/{0}-engine-dbg
-%attr(755, root, root) /usr/bin/{0}-engine-no-root
-%attr(755, root, root) /usr/lib/{0}/pydaw/python/libpydaw/pydaw_paulstretch.py
-%attr(755, root, root) /usr/lib/{0}/pydaw/python/musikernel.py
-%attr(755, root, root) /usr/lib/{0}/sbsms/bin/sbsms
-%attr(755, root, root) /usr/lib/{0}/pydaw/python/libpydaw/project_recover.py
-%attr(755, root, root) /usr/lib/{0}/pydaw/python/libpydaw/pydaw_device_dialog.py
+%attr(755, root, root) %{{_bindir}}/{0}
+%attr(755, root, root) %{{_bindir}}/{0}_render
+%attr(755, root, root) %{{_bindir}}/{0}_render-dbg
+%attr(755, root, root) %{{_bindir}}/{0}-engine-dbg
+%attr(755, root, root) %{{_bindir}}/{0}-engine-no-root
+%attr(755, root, root) %{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_paulstretch.py
+%attr(755, root, root) %{{_libdir}}/{0}/pydaw/python/musikernel.py
+%attr(755, root, root) %{{_libdir}}/{0}/sbsms/bin/sbsms
+%attr(755, root, root) %{{_libdir}}/{0}/pydaw/python/libpydaw/project_recover.py
+%attr(755, root, root) %{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_device_dialog.py
 
-/usr/lib/{0}/pydaw/mkengine/{0}.so
-/usr/lib/{0}/pydaw/python/edmnext.py
-/usr/lib/{0}/presets/MODULEX.mkp
-/usr/lib/{0}/presets/RAYV.mkp
-/usr/lib/{0}/presets/WAYV.mkp
-/usr/lib/{0}/pydaw/python/libpydaw/__init__.py
-/usr/lib/{0}/pydaw/python/libpydaw/liblo.cpython-{4}m.so
-/usr/lib/{0}/pydaw/python/libpydaw/midicomp
-/usr/lib/{0}/pydaw/python/libpydaw/portaudio.py
-/usr/lib/{0}/pydaw/python/libpydaw/portmidi.py
-/usr/lib/{0}/pydaw/python/libedmnext/gradients.py
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_history.py
-/usr/lib/{0}/pydaw/python/libedmnext/osc.py
-/usr/lib/{0}/pydaw/python/libedmnext/project.py
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_util.py
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_widgets.py
-/usr/lib/{0}/pydaw/python/libpydaw/staging.py
-/usr/lib/{0}/pydaw/python/libpydaw/super_formant_maker.py
-/usr/lib/{0}/pydaw/python/libpydaw/translate.py
-/usr/lib/{0}/major-version.txt
-/usr/lib/{0}/minor-version.txt
-/usr/lib/{0}/themes/default/drop-down.png
-/usr/lib/{0}/themes/default/euphoria.png
-/usr/lib/{0}/themes/default/h-fader.png
-/usr/lib/{0}/themes/default/mute-off.png
-/usr/lib/{0}/themes/default/mute-on.png
-/usr/lib/{0}/themes/default/play-off.png
-/usr/lib/{0}/themes/default/play-on.png
-/usr/lib/{0}/themes/default/pydaw-knob.png
-/usr/lib/{0}/themes/default/rayv.png
-/usr/lib/{0}/themes/default/rec-off.png
-/usr/lib/{0}/themes/default/rec-on.png
-/usr/lib/{0}/themes/default/record-off.png
-/usr/lib/{0}/themes/default/record-on.png
-/usr/lib/{0}/themes/default/solo-off.png
-/usr/lib/{0}/themes/default/solo-on.png
-/usr/lib/{0}/themes/default/spinbox-down.png
-/usr/lib/{0}/themes/default/spinbox-up.png
-/usr/lib/{0}/themes/default/stop-off.png
-/usr/lib/{0}/themes/default/stop-on.png
-/usr/lib/{0}/themes/default/default.pytheme
-/usr/lib/{0}/themes/default/v-fader.png
-/usr/share/applications/{0}.desktop
-/usr/share/doc/{0}/copyright
-/usr/share/pixmaps/{0}.png
-#/usr/share/locale/pt_PT/LC_MESSAGES/{0}.mo
-#/usr/share/locale/de/LC_MESSAGES/{0}.mo
-#/usr/share/locale/fr/LC_MESSAGES/{0}.mo
-/usr/lib/{0}/pydaw/python/splash.png
-/usr/lib/{0}/pydaw/python/wavefile/__init__.py
-/usr/lib/{0}/pydaw/python/wavefile/libsndfile.py
-/usr/lib/{0}/pydaw/python/wavefile/wavefile.py
+%{{_libdir}}/{0}/pydaw/mkengine/{0}.so
+%{{_libdir}}/{0}/pydaw/python/edmnext.py
+%{{_libdir}}/{0}/presets/MODULEX.mkp
+%{{_libdir}}/{0}/presets/RAYV.mkp
+%{{_libdir}}/{0}/presets/WAYV.mkp
+%{{_libdir}}/{0}/pydaw/python/libpydaw/__init__.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/liblo.cpython-{4}m.so
+%{{_libdir}}/{0}/pydaw/python/libpydaw/midicomp
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portaudio.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portmidi.py
+%{{_libdir}}/{0}/pydaw/python/libedmnext/gradients.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_history.py
+%{{_libdir}}/{0}/pydaw/python/libedmnext/osc.py
+%{{_libdir}}/{0}/pydaw/python/libedmnext/project.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_util.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_widgets.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/staging.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/super_formant_maker.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/translate.py
+%{{_libdir}}/{0}/major-version.txt
+%{{_libdir}}/{0}/minor-version.txt
+%{{_libdir}}/{0}/themes/default/drop-down.png
+%{{_libdir}}/{0}/themes/default/euphoria.png
+%{{_libdir}}/{0}/themes/default/h-fader.png
+%{{_libdir}}/{0}/themes/default/mute-off.png
+%{{_libdir}}/{0}/themes/default/mute-on.png
+%{{_libdir}}/{0}/themes/default/play-off.png
+%{{_libdir}}/{0}/themes/default/play-on.png
+%{{_libdir}}/{0}/themes/default/pydaw-knob.png
+%{{_libdir}}/{0}/themes/default/rayv.png
+%{{_libdir}}/{0}/themes/default/rec-off.png
+%{{_libdir}}/{0}/themes/default/rec-on.png
+%{{_libdir}}/{0}/themes/default/record-off.png
+%{{_libdir}}/{0}/themes/default/record-on.png
+%{{_libdir}}/{0}/themes/default/solo-off.png
+%{{_libdir}}/{0}/themes/default/solo-on.png
+%{{_libdir}}/{0}/themes/default/spinbox-down.png
+%{{_libdir}}/{0}/themes/default/spinbox-up.png
+%{{_libdir}}/{0}/themes/default/stop-off.png
+%{{_libdir}}/{0}/themes/default/stop-on.png
+%{{_libdir}}/{0}/themes/default/default.pytheme
+%{{_libdir}}/{0}/themes/default/v-fader.png
+%{{_datadir}}/applications/{0}.desktop
+%{{_datadir}}/doc/{0}/copyright
+%{{_datadir}}/pixmaps/{0}.png
+#%{{_datadir}}/locale/pt_PT/LC_MESSAGES/{0}.mo
+#%{{_datadir}}/locale/de/LC_MESSAGES/{0}.mo
+#%{{_datadir}}/locale/fr/LC_MESSAGES/{0}.mo
+%{{_libdir}}/{0}/pydaw/python/splash.png
+%{{_libdir}}/{0}/pydaw/python/wavefile/__init__.py
+%{{_libdir}}/{0}/pydaw/python/wavefile/libsndfile.py
+%{{_libdir}}/{0}/pydaw/python/wavefile/wavefile.py
 
 
-/usr/lib/{0}/pydaw/python/libpydaw/__init__.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/__init__.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/portaudio.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/portaudio.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/portmidi.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/portmidi.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/project_recover.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/project_recover.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_device_dialog.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_device_dialog.pyo
-/usr/lib/{0}/pydaw/python/libedmnext/gradients.pyc
-/usr/lib/{0}/pydaw/python/libedmnext/gradients.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_history.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_history.pyo
-/usr/lib/{0}/pydaw/python/libedmnext/osc.pyc
-/usr/lib/{0}/pydaw/python/libedmnext/osc.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_paulstretch.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_paulstretch.pyo
-/usr/lib/{0}/pydaw/python/libedmnext/project.pyc
-/usr/lib/{0}/pydaw/python/libedmnext/project.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_util.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_util.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_widgets.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/pydaw_widgets.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/staging.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/staging.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/super_formant_maker.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/super_formant_maker.pyo
-/usr/lib/{0}/pydaw/python/edmnext.pyc
-/usr/lib/{0}/pydaw/python/edmnext.pyo
-/usr/lib/{0}/pydaw/python/libpydaw/translate.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/translate.pyo
-/usr/lib/{0}/pydaw/python/wavefile/__init__.pyc
-/usr/lib/{0}/pydaw/python/wavefile/__init__.pyo
-/usr/lib/{0}/pydaw/python/wavefile/libsndfile.pyc
-/usr/lib/{0}/pydaw/python/wavefile/libsndfile.pyo
-/usr/lib/{0}/pydaw/python/wavefile/wavefile.pyc
-/usr/lib/{0}/pydaw/python/wavefile/wavefile.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portaudio.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portaudio.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portmidi.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/portmidi.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/project_recover.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/project_recover.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_device_dialog.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_device_dialog.pyo
+%{{_libdir}}/{0}/pydaw/python/libedmnext/gradients.pyc
+%{{_libdir}}/{0}/pydaw/python/libedmnext/gradients.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_history.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_history.pyo
+%{{_libdir}}/{0}/pydaw/python/libedmnext/osc.pyc
+%{{_libdir}}/{0}/pydaw/python/libedmnext/osc.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_paulstretch.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_paulstretch.pyo
+%{{_libdir}}/{0}/pydaw/python/libedmnext/project.pyc
+%{{_libdir}}/{0}/pydaw/python/libedmnext/project.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_util.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_util.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_widgets.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/pydaw_widgets.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/staging.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/staging.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/super_formant_maker.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/super_formant_maker.pyo
+%{{_libdir}}/{0}/pydaw/python/edmnext.pyc
+%{{_libdir}}/{0}/pydaw/python/edmnext.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/translate.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/translate.pyo
+%{{_libdir}}/{0}/pydaw/python/wavefile/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/wavefile/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/wavefile/libsndfile.pyc
+%{{_libdir}}/{0}/pydaw/python/wavefile/libsndfile.pyo
+%{{_libdir}}/{0}/pydaw/python/wavefile/wavefile.pyc
+%{{_libdir}}/{0}/pydaw/python/wavefile/wavefile.pyo
 
-/usr/lib/{0}/pydaw/python/libpydaw/strings.py
-/usr/lib/{0}/pydaw/python/libpydaw/strings.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/strings.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/strings.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/strings.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/strings.pyo
 
-/usr/lib/{0}/pydaw/python/libpydaw/scales.py
-/usr/lib/{0}/pydaw/python/libpydaw/scales.pyc
-/usr/lib/{0}/pydaw/python/libpydaw/scales.pyo
+%{{_libdir}}/{0}/pydaw/python/libpydaw/scales.py
+%{{_libdir}}/{0}/pydaw/python/libpydaw/scales.pyc
+%{{_libdir}}/{0}/pydaw/python/libpydaw/scales.pyo
 
-/usr/lib/{0}/pydaw/python/libedmnext/strings.py
-/usr/lib/{0}/pydaw/python/libedmnext/strings.pyc
-/usr/lib/{0}/pydaw/python/libedmnext/strings.pyo
+%{{_libdir}}/{0}/pydaw/python/libedmnext/strings.py
+%{{_libdir}}/{0}/pydaw/python/libedmnext/strings.pyc
+%{{_libdir}}/{0}/pydaw/python/libedmnext/strings.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/__init__.py
-/usr/lib/{0}/pydaw/python/mkplugins/__init__.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/__init__.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/euphoria.py
-/usr/lib/{0}/pydaw/python/mkplugins/euphoria.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/euphoria.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/modulex.py
-/usr/lib/{0}/pydaw/python/mkplugins/modulex.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/modulex.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/rayv.py
-/usr/lib/{0}/pydaw/python/mkplugins/rayv.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/rayv.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/wayv.py
-/usr/lib/{0}/pydaw/python/mkplugins/wayv.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/wayv.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/__init__.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/euphoria.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/euphoria.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/euphoria.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/modulex.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/modulex.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/modulex.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/rayv.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/rayv.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/rayv.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/wayv.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/wayv.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/wayv.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/mk_delay.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_delay.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_delay.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/mk_eq.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_eq.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_eq.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/simple_fader.py
-/usr/lib/{0}/pydaw/python/mkplugins/simple_fader.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/simple_fader.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/simple_reverb.py
-/usr/lib/{0}/pydaw/python/mkplugins/simple_reverb.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/simple_reverb.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/trigger_fx.py
-/usr/lib/{0}/pydaw/python/mkplugins/trigger_fx.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/trigger_fx.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_delay.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_delay.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_delay.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_eq.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_eq.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_eq.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_fader.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_fader.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_fader.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_reverb.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_reverb.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/simple_reverb.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/trigger_fx.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/trigger_fx.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/trigger_fx.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/sidechain_comp.py
-/usr/lib/{0}/pydaw/python/mkplugins/sidechain_comp.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/sidechain_comp.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/sidechain_comp.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/sidechain_comp.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/sidechain_comp.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/mk_channel.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_channel.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_channel.pyo
-/usr/lib/{0}/pydaw/python/mkplugins/xfade.py
-/usr/lib/{0}/pydaw/python/mkplugins/xfade.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/xfade.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_channel.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_channel.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_channel.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/xfade.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/xfade.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/xfade.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/mk_compressor.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_compressor.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_compressor.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_compressor.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_compressor.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_compressor.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/mk_vocoder.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_vocoder.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_vocoder.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_vocoder.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_vocoder.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_vocoder.pyo
 
-/usr/lib/{0}/pydaw/python/musikernel.pyc
-/usr/lib/{0}/pydaw/python/musikernel.pyo
+%{{_libdir}}/{0}/pydaw/python/musikernel.pyc
+%{{_libdir}}/{0}/pydaw/python/musikernel.pyo
 
-/usr/lib/{0}/pydaw/python/mkplugins/mk_limiter.py
-/usr/lib/{0}/pydaw/python/mkplugins/mk_limiter.pyc
-/usr/lib/{0}/pydaw/python/mkplugins/mk_limiter.pyo
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_limiter.py
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_limiter.pyc
+%{{_libdir}}/{0}/pydaw/python/mkplugins/mk_limiter.pyo
 
-/usr/lib/{0}/pydaw/python/libmk/__init__.py
-/usr/lib/{0}/pydaw/python/libmk/__init__.pyc
-/usr/lib/{0}/pydaw/python/libmk/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/libmk/__init__.py
+%{{_libdir}}/{0}/pydaw/python/libmk/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/libmk/__init__.pyo
 
-/usr/lib/{0}/pydaw/python/libmk/mk_project.py
-/usr/lib/{0}/pydaw/python/libmk/mk_project.pyc
-/usr/lib/{0}/pydaw/python/libmk/mk_project.pyo
+%{{_libdir}}/{0}/pydaw/python/libmk/mk_project.py
+%{{_libdir}}/{0}/pydaw/python/libmk/mk_project.pyc
+%{{_libdir}}/{0}/pydaw/python/libmk/mk_project.pyo
 
-/usr/lib/{0}/pydaw/python/libedmnext/__init__.py
-/usr/lib/{0}/pydaw/python/libedmnext/__init__.pyc
-/usr/lib/{0}/pydaw/python/libedmnext/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/libedmnext/__init__.py
+%{{_libdir}}/{0}/pydaw/python/libedmnext/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/libedmnext/__init__.pyo
 
-/usr/lib/{0}/pydaw/python/wavenext.py
-/usr/lib/{0}/pydaw/python/wavenext.pyc
-/usr/lib/{0}/pydaw/python/wavenext.pyo
+%{{_libdir}}/{0}/pydaw/python/wavenext.py
+%{{_libdir}}/{0}/pydaw/python/wavenext.pyc
+%{{_libdir}}/{0}/pydaw/python/wavenext.pyo
 
-/usr/lib/{0}/pydaw/python/dawnext.py
-/usr/lib/{0}/pydaw/python/dawnext.pyc
-/usr/lib/{0}/pydaw/python/dawnext.pyo
-/usr/lib/{0}/pydaw/python/libdawnext/__init__.py
-/usr/lib/{0}/pydaw/python/libdawnext/__init__.pyc
-/usr/lib/{0}/pydaw/python/libdawnext/__init__.pyo
-/usr/lib/{0}/pydaw/python/libdawnext/gradients.py
-/usr/lib/{0}/pydaw/python/libdawnext/gradients.pyc
-/usr/lib/{0}/pydaw/python/libdawnext/gradients.pyo
-/usr/lib/{0}/pydaw/python/libdawnext/osc.py
-/usr/lib/{0}/pydaw/python/libdawnext/osc.pyc
-/usr/lib/{0}/pydaw/python/libdawnext/osc.pyo
-/usr/lib/{0}/pydaw/python/libdawnext/project.py
-/usr/lib/{0}/pydaw/python/libdawnext/project.pyc
-/usr/lib/{0}/pydaw/python/libdawnext/project.pyo
-/usr/lib/{0}/pydaw/python/libdawnext/strings.py
-/usr/lib/{0}/pydaw/python/libdawnext/strings.pyc
-/usr/lib/{0}/pydaw/python/libdawnext/strings.pyo
+%{{_libdir}}/{0}/pydaw/python/dawnext.py
+%{{_libdir}}/{0}/pydaw/python/dawnext.pyc
+%{{_libdir}}/{0}/pydaw/python/dawnext.pyo
+%{{_libdir}}/{0}/pydaw/python/libdawnext/__init__.py
+%{{_libdir}}/{0}/pydaw/python/libdawnext/__init__.pyc
+%{{_libdir}}/{0}/pydaw/python/libdawnext/__init__.pyo
+%{{_libdir}}/{0}/pydaw/python/libdawnext/gradients.py
+%{{_libdir}}/{0}/pydaw/python/libdawnext/gradients.pyc
+%{{_libdir}}/{0}/pydaw/python/libdawnext/gradients.pyo
+%{{_libdir}}/{0}/pydaw/python/libdawnext/osc.py
+%{{_libdir}}/{0}/pydaw/python/libdawnext/osc.pyc
+%{{_libdir}}/{0}/pydaw/python/libdawnext/osc.pyo
+%{{_libdir}}/{0}/pydaw/python/libdawnext/project.py
+%{{_libdir}}/{0}/pydaw/python/libdawnext/project.pyc
+%{{_libdir}}/{0}/pydaw/python/libdawnext/project.pyo
+%{{_libdir}}/{0}/pydaw/python/libdawnext/strings.py
+%{{_libdir}}/{0}/pydaw/python/libdawnext/strings.pyc
+%{{_libdir}}/{0}/pydaw/python/libdawnext/strings.pyo
 
 
 %doc
