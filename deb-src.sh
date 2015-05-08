a8242eef1194434fed7521eda9e4c502d2525b7a
MusiKernel Team <musikernel@nospam.org>
2015-05-08 19:44:14 -0400

    Fixes to the launchpad PPA script

--- deb-src.sh
+++ deb-src.sh
@@ -1,12 +1,13 @@
-git pull
-MAJOR_VERSION=$(cat src/major-version.txt)
-MINOR_VERSION=$(cat src/minor-version.txt)
-FULL_VERSION=$(MAJOR_VERSION)-$(MINOR_VERSION)
-CHANGES_FILE=$(MAJOR_VERSION)_$(MINOR_VERSION)-1_source.changes
+#!/bin/sh
+
+MAJOR_VERSION=`cat src/major-version.txt`
+MINOR_VERSION=`cat src/minor-version.txt`
+FULL_VERSION="$MAJOR_VERSION-$MINOR_VERSION"
+CHANGES_FILE="$MAJOR_VERSION_$MINOR_VERSION-1_source.changes"
 
 cp -r src $FULL_VERSION
 cd $FULL_VERSION
 debuild -S -sa -k412C4B95
 cd ..
-dput ppa:musikernel/musikernel $(CHANGES_FILE)
+dput ppa:musikernel/musikernel1-staging $CHANGES_FILE
 
