6e069b106937932814b5eb54433a55f181ef9ea5
MusiKernel Team <musikernel@nospam.org>
2015-05-09 11:51:54 -0400

    Remove Scipy from the Fedora dependency installer script, it hasn't been a dependency for months now...

--- src/fedora_deps.sh
+++ src/fedora_deps.sh
@@ -1,6 +1,6 @@
 #dependencies
 sudo yum install python3-qt5 gcc alsa-lib-devel liblo-devel \
-libsndfile-devel gcc-c++ git python3-numpy python3-scipy \
+libsndfile-devel gcc-c++ git python3-numpy \
 fftw-devel portmidi-devel portaudio-devel rubberband python3-devel \
 @development-tools fedora-packager livecd-tools spin-kickstarts \
 system-config-kickstart vorbis-tools gettext
