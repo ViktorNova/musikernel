1a8f12cec8d8fcf494f438f8d6e22b0e57d6ff76
MusiKernel Team <musikernel@nospam.org>
2015-05-21 19:57:01 -0400

    Dammit!

--- src/pydaw/python/libmk/mk_project.py
+++ src/pydaw/python/libmk/mk_project.py
@@ -415,8 +415,6 @@ class MkProject(libmk.AbstractProject):
             f_uid_dict = a_uid_dict
         if pydaw_util.IS_WINDOWS:
             f_path = str(a_path)
-            if f_path[1] == ":":
-                f_path = f_path.replace(":", "", 1)
         else:
             f_path = str(a_path).replace("//", "/")
         if a_cp:
